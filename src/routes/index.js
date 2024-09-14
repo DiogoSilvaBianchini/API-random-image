d340e6d82565c7b90dbf7aa78fb386d560a8b5da
unknown <diogosilvabianchini@gmail.com>
2024-09-14 11:50:13 -0300

    Adição das rota de teste

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,11 @@ import express from 'express'
 import {unsplash} from '../config/unsplashConfig.js'
 const router = express.Router()
 
-router.get("/", async (req,res) => {
+router.get("/", (req,res) => {
+    return res.status(200).send("hellow word")
+})
+
+router.get("/img", async (req,res) => {
     const request = await unsplash.photos.getRandom({
         query: 'dog'
     })
